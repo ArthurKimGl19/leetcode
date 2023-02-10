a642ff27267add8335677f7bfb0cc83d1d94d9ec
ArthurKimGl19 <arthur.kim.fse@gmail.com>
2023-02-09 16:53:21 -0800

    Complete problem 21 merge two sorted lists

---
+++ 21_merge_two_sorted_lists.js
@@ -0,0 +1,36 @@
+var mergeTwoLists = function(list1, list2) {
+    if (!list1 && !list2) return null
+    else if (!list1) return list2
+    else if (!list2) return list1;
+
+    let output = null;
+    let currentNode = null;
+
+    while (list1 && list2){
+        if (list1.val <= list2.val){
+            if (!output){
+                output = list1;
+            } else {
+                currentNode.next = list1;
+            }
+            currentNode = list1;
+            list1 = list1.next;
+        } else {
+            if (!output){
+                output = list2;
+            } else {
+                currentNode.next = list2;
+            }
+            currentNode = list2;
+            list2 = list2.next;
+        }
+    }
+    //if list 1 or list 2 are null now, we have to add the rest of other list to current output;
+    if (!list1 && list2){
+        currentNode.next = list2;
+    } else if (!list2 && list1){
+        currentNode.next = list1;
+    }
+    return output
+    //Time complexity O(n), Space complexity O(n);
+};
