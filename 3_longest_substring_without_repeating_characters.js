0c39b1d4b9f47e20bac55cb41a54a89fe05b803e
ArthurKimGl19 <arthur.kim.fse@gmail.com>
2023-02-27 13:56:02 -0800

    Complete problem longest substring without repating characters optimized using set and not checking every substring

--- 3_longest_substring_without_repeating_characters.js
+++ 3_longest_substring_without_repeating_characters.js
@@ -1,33 +1,47 @@
 var lengthOfLongestSubstring = function(s) {
-    if (s.length === 0){
-        return 0;
-    } else if (s.length === 1){
-        return 1;
-    }
-
-    let storage = new Set();
-
-    let left = 0;
-    let right = left+1;
-    storage.add(s[left]);
-    let currentCount = 1;
+    // if (s.length === 0){
+    //     return 0;
+    // } else if (s.length === 1){
+    //     return 1;
+    // }
+    //
+    // let storage = new Set();
+    //
+    // let left = 0;
+    // let right = left+1;
+    // storage.add(s[left]);
+    // let currentCount = 1;
+    // let maxCount = 0;
+    //
+    // while (left < s.length - 1){
+    //     if (!storage.has(s[right]) && right <= s.length - 1){
+    //         storage.add(s[right]);
+    //         currentCount++;
+    //         right++;
+    //     } else {
+    //         maxCount = Math.max(currentCount, maxCount);
+    //         currentCount = 0;
+    //         left++;
+    //         storage = new Set();
+    //         storage.add(s[left]);
+    //         currentCount++;
+    //         right = left + 1;
+    //     }
+    // }
+    // return maxCount
+    // //Time Complexity O(n), Space Complexity O(n);
     let maxCount = 0;
+    let start = 0;
+    const set = new Set();
 
-    while (left < s.length - 1){
-        if (!storage.has(s[right]) && right <= s.length - 1){
-            storage.add(s[right]);
-            currentCount++;
-            right++;
-        } else {
-            maxCount = Math.max(currentCount, maxCount);
-            currentCount = 0;
-            left++;
-            storage = new Set();
-            storage.add(s[left]);
-            currentCount++;
-            right = left + 1;
+    for (let end = 0; end < s.length; end++){
+        while (set.has(s[end])){
+            set.delete(s[start]);
+            start++;
         }
+        set.add(s[end]);
+        maxCount = Math.max(maxCount, end - start + 1);
     }
-    return maxCount
+    return maxCount;
     //Time Complexity O(n), Space Complexity O(n);
 };
