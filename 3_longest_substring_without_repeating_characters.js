c76fb464e4bf23c43eae38fb5e33526416ba46e2
ArthurKimGl19 <arthur.kim.fse@gmail.com>
2023-03-01 11:49:44 -0800

    Complete problem 3 longest substring without reapting characters

--- 3_longest_substring_without_repeating_characters.js
+++ 3_longest_substring_without_repeating_characters.js
@@ -1,47 +1,33 @@
 var lengthOfLongestSubstring = function(s) {
-    // if (s.length === 0){
-    //     return 0;
-    // } else if (s.length === 1){
-    //     return 1;
-    // }
-    //
-    // let storage = new Set();
-    //
-    // let left = 0;
-    // let right = left+1;
-    // storage.add(s[left]);
-    // let currentCount = 1;
-    // let maxCount = 0;
-    //
-    // while (left < s.length - 1){
-    //     if (!storage.has(s[right]) && right <= s.length - 1){
-    //         storage.add(s[right]);
-    //         currentCount++;
-    //         right++;
-    //     } else {
-    //         maxCount = Math.max(currentCount, maxCount);
-    //         currentCount = 0;
-    //         left++;
-    //         storage = new Set();
-    //         storage.add(s[left]);
-    //         currentCount++;
-    //         right = left + 1;
-    //     }
-    // }
-    // return maxCount
-    // //Time Complexity O(n), Space Complexity O(n);
-    let maxCount = 0;
-    let start = 0;
-    const set = new Set();
-
-    for (let end = 0; end < s.length; end++){
-        while (set.has(s[end])){
-            set.delete(s[start]);
-            start++;
+    var lengthOfLongestSubstring = function(s) {
+        if (s.length === 0){
+            return 0;
+        } else if (s.length === 1){
+            return 1;
         }
-        set.add(s[end]);
-        maxCount = Math.max(maxCount, end - start + 1);
-    }
-    return maxCount;
+
+        let front = 0;
+        let currentCount = 0;
+        let maxCount = 0;
+        const set = new Set();
+
+        for (let i = 0; i < s.length; i++){
+            if (!set.has(s[i])){
+                set.add(s[i]);
+                currentCount++;
+                maxCount = Math.max(maxCount, currentCount);
+            } else {
+                while (set.has(s[i])){
+                    set.delete(s[front]);
+                    front++;
+                    currentCount--;
+                }
+                set.add(s[i]);
+                currentCount++;
+                maxCount = Math.max(maxCount, currentCount);
+            }
+        }
+        return maxCount;
+    };
     //Time Complexity O(n), Space Complexity O(n);
 };
