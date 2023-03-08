755885950cfc7056dc490c2011cec7d62ce7961b
ArthurKimGl19 <arthur.kim.fse@gmail.com>
2023-03-08 13:30:35 -0800

    Attempt problem 378 kth smallest element in a sorted matrix, improvement on previous brute force solution but not optimal solution

--- 378_kth_smallest_element_in_a_sorted_matrix.js
+++ 378_kth_smallest_element_in_a_sorted_matrix.js
@@ -1,13 +1,24 @@
 var kthSmallest = function(matrix, k) {
-    const storage = [];
+    // let storage = [];
 
-    for (let i = 0; i < matrix.length; i++){
-        for (let j = 0; j < matrix[i].length; j++){
-            storage.push(matrix[i][j]);
-        }
-    }
-    storage.sort((a, b) => a-b);
-    return storage[k-1];
-    //Time complexity O(n) space complexity (On^2)
-    //There is a binary search way to keep mem complexity to O(1)
+    // for (let i = 0; i < matrix.length; i++){
+    //     storage = storage.concat(matrix[i]);
+    // }
+    // storage.sort((a,b) => a-b);
+    // return storage[k-1];
+    //brute force and creates brand new array each time storage concat is invoked
+
+    // const storage = matrix.flat();
+    // storage.sort((a,b) => a-b);
+    // return storage[k-1];
+    //better brute force, flat is still O(n^2) but memory is O(n)
+
+    //change to string and remove [], add [] to front and back
+    let newArray = JSON.stringify(matrix).replace(/[\[\]']+/g, '');
+    newArray = "[" + newArray + "]";
+    newArray = JSON.parse(newArray);
+    newArray.sort((a,b) => a-b);
+    return newArray[k-1];
+    //Time complexity O(n logn) with native sort, space is O(n);
+    //Optimal solution is using binary search.
 };
