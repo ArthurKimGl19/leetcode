bbc2270e192f1cb8d9494e94d1c8e940fa9238eb
ArthurKimGl19 <arthur.kim.fse@gmail.com>
2023-03-17 14:22:52 -0700

    Complete problem 3 longest substring withouth repeating characters with different solution - Looks a bit cleaner compared to first approach

--- 3_longest_substring_without_repeating_characters.js
+++ 3_longest_substring_without_repeating_characters.js
@@ -1,4 +1,3 @@
-var lengthOfLongestSubstring = function(s) {
     var lengthOfLongestSubstring = function(s) {
         if (s.length === 0){
             return 0;
@@ -28,6 +27,42 @@ var lengthOfLongestSubstring = function(s) {
             }
         }
         return maxCount;
+        //Time Complexity O(n), Space Complexity O(n);
+
     };
-    //Time Complexity O(n), Space Complexity O(n);
-};
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    var lengthOfLongestSubstring = function(s) {
+        if (s.length === 0){
+            return 0;
+        } else if (s.length === 1){
+            return 1;
+        }
+
+        let pointer = 0;
+        let max = 0;
+        const storage = {};
+
+        for (let i = 0; i < s.length; i++){
+            //if not in storage
+            //add and set value to be i + 1
+            //set max to be i - p + 1 or max whatever is larger
+            //else if in storage
+            //set pointer to be value at s[i] in storage or pointer whatever is greater
+            //set max to be i - p + 1 or max whatever is larger
+            //update value of s[i] in storage to be i + 1
+            if (storage[s[i]] === undefined){
+                storage[s[i]] = i + 1;
+                max = Math.max(max, i - pointer + 1);
+            } else {
+                pointer = Math.max(storage[s[i]], pointer);
+                max = Math.max(max, i - pointer + 1);
+                storage[s[i]] = i + 1;
+            }
+        }
+        return max;
+        //return max
+        //Different Implementation : Time Complexity O(n), Space Complexity O(n);
+    };
