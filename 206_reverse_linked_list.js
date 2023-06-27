fa97cd3082f702a916a94135f7f49739b31701a7
ArthurKimGl19 <arthur.kim.fse@gmail.com>
2023-06-27 09:59:27 -0700

    Complete problem 206 reverse linkedin list

--- 206_reverse_linked_list.js
+++ 206_reverse_linked_list.js
@@ -9,55 +9,29 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-function traverseAndAdd(node){
-    const output = [];
-    while (node){
-        output.push(node);
-        node = node.next;
-    }
-    return output;
-}
-
 var reverseList = function(head) {
-    if (!head) return head;
-    //iterative approach
-    // const queue = traverseAndAdd(head);
-    // let reversed, currentNode;
+    if (!head || !head.next){
+        return head;
+    }
 
-    // for (let i = queue.length - 1; i >= 0; i--){
-    //     if (!reversed){
-    //         reversed = currentNode = queue[i];
-    //     } else {
-    //         //set curretNode's next to be queue i and set currentNode to be queue i
-    //         currentNode.next = queue[i];
-    //         currentNode = queue[i];
-    //         if (i === 0){
-    //             currentNode.next = null;
-    //         }
-    //     }
-    // }
-    // return reversed;
-
-    //iterative approach optimized
-    //without use of array; space complexity is now O(1)
-    //edge case for this one is to set the last node's next value to be null in reversed list
-    // let previousNode, currentNode, reversed;
-    // previousNode = head;
-    // currentNode = previousNode.next;
-    // previousNode.next = null;
-    // if (!currentNode){
-    //     previousNode.next = null;
-    //     return previousNode;
-    // }
-    // while (currentNode){
-    //     if (currentNode.next === null){
-    //         reversed = currentNode;
-    //     }
-
-    //     const nextNode = currentNode.next;
-    //     currentNode.next = previousNode;
-    //     previousNode = currentNode;
-    //     currentNode = nextNode;
-    // }
-    // return reversed;
+    const storage = [];
+    let currentNode = head;
+    while (currentNode){
+        storage.push(currentNode);
+        currentNode = currentNode.next;
+    }
+    let output;
+    while (storage.length > 0){
+        const poppedNode = storage.pop();
+        if (!output){
+            output = poppedNode;
+            currentNode = output;
+        } else {
+            currentNode.next = poppedNode;
+            currentNode = poppedNode;
+        }
+    }
+    currentNode.next = null;
+    return output;
+    //Time Complexity O(n), Space Complexity O(n)
 };
